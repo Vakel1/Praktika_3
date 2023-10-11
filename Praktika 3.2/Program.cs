2acee7ba02fc17947b1250ae668689491bc40f09
Vakel <TURBOUser@DESKTOP-JS6F07E>
2023-10-11 15:33:30 +0300

    Добавьте файлы проекта.

---
+++ Praktika 3.2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Notice
+{
+    // Класс для уведомлений
+    public class Notice
+    {
+        // Объявляем делегат для события
+        public delegate void NotificationHandler(string message);
+
+        // Определяем события для разных типов уведомлений
+        public event NotificationHandler MessageEvent; // Событие для сообщений
+        public event NotificationHandler CallEvent;    // Событие для звонков
+        public event NotificationHandler EmailEvent;   // Событие для электронной почты
+
+        // Метод для отправки сообщения
+        public void SendMessage(string message)
+        {
+            Console.WriteLine("Отправка сообщения: " + message);
+
+            MessageEvent?.Invoke(message); // Вызываем событие для сообщений
+        }
+
+        // Метод для отправки звонка
+        public void MakeCall(string message)
+        {
+            Console.WriteLine("Звонок: " + message);
+
+            CallEvent?.Invoke(message); // Вызываем событие для звонков
+        }
+
+        // Метод для отправки электронной почты
+        public void SendEmail(string message)
+        {
+            Console.WriteLine("Отправка электронной почты: " + message);
+
+            EmailEvent?.Invoke(message); // Вызываем событие для электронной почты
+        }
+    }
+
+    class Noticed
+    {
+        static void Main(string[] args)
+        {
+            Notice notificationSystem = new Notice(); // Создаем экземпляр класса Notice
+
+            // Регистрируем обработчики событий
+            notificationSystem.MessageEvent += MessageHandler; // Обработчик для сообщений
+            notificationSystem.CallEvent += CallHandler;       // Обработчик для звонков
+            notificationSystem.EmailEvent += EmailHandler;     // Обработчик для электронной почты
+
+            while (true)
+            {
+                Console.WriteLine("Выберите тип уведомления (1 - сообщение, 2 - звонок, 3 - электронная почта, 4 - выход):");
+                string choice = Console.ReadLine();
+
+                if (choice == "4")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Введите текст уведомления:");
+                string message = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        notificationSystem.SendMessage(message);
+                        break;
+                    case "2":
+                        notificationSystem.MakeCall(message);
+                        break;
+                    case "3":
+                        notificationSystem.SendEmail(message);
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный выбор. Пожалуйста, выберите 1, 2, 3 или 4.");
+                        break;
+                }
+            }
+        }
+
+        // Обработчик для сообщений
+        static void MessageHandler(string message)
+        {
+            Console.WriteLine("Обработчик для сообщений: " + message);
+        }
+
+        // Обработчик для звонков
+        static void CallHandler(string message)
+        {
+            Console.WriteLine("Обработчик для звонков: " + message);
+        }
+
+        // Обработчик для электронной почты
+        static void EmailHandler(string message)
+        {
+            Console.WriteLine("Обработчик для электронной почты: " + message);
+        }
+    }
+}
---
+++ Praktika 3/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Shape
+{
+    // Базовый класс Shape
+    class Shape
+    {
+        // Виртуальный метод для расчета площади, который будет переопределяться в производных классах
+        public virtual double CalculateArea()
+        {
+            return 0; // По умолчанию площадь равна 0
+        }
+    }
+
+    // Класс Circle, представляющий круг, наследует от Shape
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        // Конструктор для установки радиуса
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Переопределенный метод для расчета площади круга
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    // Класс Rectangle, представляющий прямоугольник, наследует от Shape
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        // Конструктор для установки ширины и высоты прямоугольника
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Переопределенный метод для расчета площади прямоугольника
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    // Класс Triangle, представляющий треугольник, наследует от Shape
+    class Triangle : Shape
+    {
+        public double BaseLength { get; set; }
+        public double Height { get; set; }
+
+        // Конструктор для установки длины основания и высоты треугольника
+        public Triangle(double baseLength, double height)
+        {
+            BaseLength = baseLength;
+            Height = height;
+        }
+
+        // Переопределенный метод для расчета площади треугольника
+        public override double CalculateArea()
+        {
+            return 0.5 * BaseLength * Height;
+        }
+    }
+
+    // Класс ShapeChoice для взаимодействия с пользователем
+    class ShapeChoice
+    {
+        delegate double CalculateAreaDelegate();
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите фигуру для вычисления площади: ");
+                Console.WriteLine("1. Круг");
+                Console.WriteLine("2. Прямоугольник");
+                Console.WriteLine("3. Треугольник");
+                Console.WriteLine("4. Выйти из программы");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 4)
+                {
+                    break; // Пользователь выбрал выход из программы
+                }
+
+                Shape shape = null;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Введите радиус круга: ");
+                        double radius = double.Parse(Console.ReadLine());
+                        shape = new Circle(radius);
+                        break;
+                    case 2:
+                        Console.Write("Введите ширину прямоугольника: ");
+                        double width = double.Parse(Console.ReadLine());
+                        Console.Write("Введите высоту прямоугольника: ");
+                        double height = double.Parse(Console.ReadLine());
+                        shape = new Rectangle(width, height);
+                        break;
+                    case 3:
+                        Console.Write("Введите длину основания треугольника: ");
+                        double baseLength = double.Parse(Console.ReadLine());
+                        Console.Write("Введите высоту треугольника: ");
+                        double triangleHeight = double.Parse(Console.ReadLine());
+                        shape = new Triangle(baseLength, triangleHeight);
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный выбор.");
+                        continue;
+                }
+
+                CalculateAreaDelegate shapeDelegate = shape.CalculateArea;
+                Console.WriteLine($"Площадь выбранной фигуры: {shapeDelegate()}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
